38c5d390e4324914db924af24713e32121e8f498
Jakub <jakub.misiuk@gmail.com>
2020-07-26 20:30:53 +0200

    First Commit

---
+++ src/main/java/pl/jakub/weekop/WeekopApplication.java
@@ -0,0 +1,13 @@
+package pl.jakub.weekop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekopApplication.class, args);
+    }
+
+}
